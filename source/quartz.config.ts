ea4fc585c28b1a315cc3472f9aa15fe764197b87
Clark Weckmann <15677451+clarkhacks@users.noreply.github.com>
2025-07-20 21:21:59 -0500

    Update page title and footer links for consistency

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Academy Notes",
+    pageTitle: "📝 Academy Notes",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
